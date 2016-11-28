e24f82330575d363d539b4d310ee986507f1acf8
Omkar Bolla <omkar062@gmail.com>
2016-11-28 20:05:00 +0530

    [Tool-Chain] Cross Compiler for intel 64-bit OS

---
+++ cross-tools/usr/lib/gcc/arm-buildroot-linux-uclibcgnueabihf/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/embedded/cubie/build-root/usr --sysconfdir=/embedded/cubie/build-root/etc --enable-static --target=arm-buildroot-linux-uclibcgnueabihf --with-sysroot=/embedded/cubie/build-root/usr/arm-buildroot-linux-uclibcgnueabihf/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/embedded/cubie/build-root/usr --with-mpfr=/embedded/cubie/build-root/usr --with-pkgversion='Buildroot 2015.05' --with-bugurl=http://bugs.buildroot.net/ --enable-target-optspace --disable-libsanitizer --enable-tls --disable-libmudflap --enable-threads --with-mpc=/embedded/cubie/build-root/usr --without-isl --without-cloog --disable-decimal-float --with-abi=aapcs-linux --with-cpu=cortex-a7 --with-fpu=neon-vfpv4 --with-float=hard --with-mode=arm --enable-languages=c,c++ --enable-poison-system-directories --with-build-time-tools=/embedded/cubie/build-root/usr/arm-buildroot-linux-uclibcgnueabihf/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a7" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
